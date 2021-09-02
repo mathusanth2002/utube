42929f581414243b8968756baa95a0d894c20881
mathusanth2002 <89956973+mathusanth2002@users.noreply.github.com>
2021-09-02 09:35:17 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,49 +1,78 @@
-import os
-
-
-class Config:
-
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-
-    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
-
-    API_ID = int(os.environ.get("API_ID"))
-
-    API_HASH = os.environ.get("API_HASH")
-
-    CLIENT_ID = os.environ.get("CLIENT_ID")
-
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
-
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-
-    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
-
-    AUTH_USERS = [BOT_OWNER, 374321319] + (
-        [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
-        if AUTH_USERS_TEXT
-        else []
-    )
-
-    VIDEO_DESCRIPTION = (
-        os.environ.get("VIDEO_DESCRIPTION", "").replace("<", "").replace(">", "")
-    )
-
-    VIDEO_CATEGORY = (
-        int(os.environ.get("VIDEO_CATEGORY")) if os.environ.get("VIDEO_CATEGORY") else 0
-    )
-
-    VIDEO_TITLE_PREFIX = os.environ.get("VIDEO_TITLE_PREFIX", "")
-
-    VIDEO_TITLE_SUFFIX = os.environ.get("VIDEO_TITLE_SUFFIX", "")
-
-    DEBUG = bool(os.environ.get("DEBUG"))
-
-    UPLOAD_MODE = os.environ.get("UPLOAD_MODE") or False
-    if UPLOAD_MODE:
-        if UPLOAD_MODE.lower() in ["private", "public", "unlisted"]:
-            UPLOAD_MODE = UPLOAD_MODE.lower()
-        else:
-            UPLOAD_MODE = False
-
-    CRED_FILE = "auth_token.txt"
+{
+  "name": "Youtube Upload Bot",
+  "description": "Telegram to YouTube Uploader Bot",
+  "repository": "https://github.com/odysseusmax/utube",
+  "logo": "https://github.com/odysseusmax/utube/blob/master/ss/logo.jpeg",
+  "keywords": ["telegram", "youtube", "uploader"],
+  "env": {
+      "BOT_TOKEN": {
+      "description": "Your Bot token. Get from https://t.me/botfather",
+      "value": "1991699635:AAEKPca5JvN4pxusGNTf-m7C4kxjK_GcIvU"
+    },
+    "SESSION_NAME": {
+      "description": "Your Bot's username.",
+      "value": "youtubeitbot",
+      "required":false
+    },
+    "API_ID": {
+      "description": "Your API_ID.  Refer project docs.",
+      "value": "5615631"
+    },
+    "API_HASH": {
+      "description": "Your API_ID. Refer project docs.",
+      "value": "f65d600ff580456871cfddc086337b7a"
+    },
+    "CLIENT_ID": {
+      "description": "Your Google CLIENT_ID. Refer project docs.",
+      "value": "921912615039-42h2eg2tsqcmbig6mae5df8vai54mbff.apps.googleusercontent.com"
+    },
+    "CLIENT_SECRET": {
+      "description": "Your Google CLIENT_SECRET. Refer project docs.",
+      "value": "GnsnVpyT3DT9cJStuexDOJ55"
+    },
+    "BOT_OWNER": {
+      "description": "Telegram id of the bot owner.",
+      "value": "1496802577"
+    },
+    "AUTH_USERS": {
+      "description": "Telegram id of authorised users, separated by comma",
+      "value": "1496802577",
+      "required":true
+    },
+    "VIDEO_DESCRIPTION": {
+      "description": "Any default description to be aded to the video.",
+      "value": "",
+      "required":false
+    },
+    "VIDEO_CATEGORY": {
+      "description": "YouTube's video category id.",
+      "value": "",
+      "required":false
+    },
+    "VIDEO_TITLE_PREFIX": {
+      "description": "Any prefix to be added to the video's title.",
+      "value": "",
+      "required":false
+    },
+    "VIDEO_TITLE_SUFFIX": {
+      "description": "Any suffix to be added to the video's title.",
+      "value": "",
+      "required":false
+    },
+    "UPLOAD_MODE": {
+      "description": "The video's privacy status. Valid values for this property are: private, public, unlisted",
+      "value": "public",
+      "required":true
+    },
+    "DEBUG": {
+      "description": "Pass any value to set logging to debug level.",
+      "value": "",
+      "required":false
+    }
+   },
+   "buildpacks": [
+    {
+      "url": "heroku/python"
+    }
+  ]
+}
