698af15528f0162cad2e6b34bf3a42148162d430
mathusanth2002 <89956973+mathusanth2002@users.noreply.github.com>
2021-09-02 08:58:57 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,21 +3,21 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("1991699635:AAEKPca5JvN4pxusGNTf-m7C4kxjK_GcIvU")
 
-    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
+    SESSION_NAME = os.environ.get("YouTubeUploader", ":memory:")
 
-    API_ID = int(os.environ.get("API_ID"))
+    API_ID = int(os.environ.get("5615631"))
 
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("f65d600ff580456871cfddc086337b7a")
 
-    CLIENT_ID = os.environ.get("CLIENT_ID")
+    CLIENT_ID = os.environ.get("921912615039-42h2eg2tsqcmbig6mae5df8vai54mbff.apps.googleusercontent.com")
 
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+    CLIENT_SECRET = os.environ.get("GnsnVpyT3DT9cJStuexDOJ55")
 
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    BOT_OWNER = int(os.environ.get("1496802577"))
 
-    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
+    AUTH_USERS_TEXT = os.environ.get("1496802577", "")
 
     AUTH_USERS = [BOT_OWNER, 374321319] + (
         [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
